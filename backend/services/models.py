1c15a10fbc6698fbd6c3d5c0492b8caea1cab596
GeorgyHub <georgy.polkanov@yandex.ru>
2025-03-31 13:15:33 +0300

    Added Singleton

--- backend/services/models.py
+++ backend/services/models.py
@@ -14,15 +14,16 @@ class Service(models.Model):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__discount_percent = self.discount_percent
+        self.__full_price = self.full_price
 
-    def save(self, *args, save_model=True, **kwargs):
+    def save(self, *args, **kwargs):
 
-        if self.discount_percent != self.__full_price:
+        if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
-                set_price.delay(self.id)
+                set_price.delay(subscription.id)
 
         return super().save(*args, **kwargs)
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -30,12 +31,12 @@ class Plan(models.Model):
         ('student', 'Student'),
         ('discount', 'Discount')
     )
+
     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
     discount_percent = models.PositiveIntegerField(default=0,
                                                    validators=[
                                                        MaxValueValidator(100)
                                                    ])
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__discount_percent = self.discount_percent
@@ -47,6 +48,7 @@ class Plan(models.Model):
                 set_price.delay(subscription.id)
 
         return super().save(*args, **kwargs)
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)

9e9eb18d7913a310d4dd4a965e2f0bfdfb70040d
GeorgyHub <georgy.polkanov@yandex.ru>
2025-04-09 19:17:16 +0300

    Added workers

--- backend/services/tasks.py
+++ backend/services/tasks.py
@@ -1,20 +1,41 @@
 import time
+import datetime
 
 from celery import shared_task
 from django.db.models import F
+from django.db import transaction
 from celery_singleton import Singleton
 
 @shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
 
-    time.sleep(5)
+    with transaction.atomic():
+        time.sleep(5)
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
-        annotated_price=F('service__full_price') -
-                     F('service__full_price') *
-                     F('plan__discount_percent') / 100.00).first()
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotated_price=F('service__full_price') -
+                         F('service__full_price') *
+                         F('plan__discount_percent') / 100.00).first()
 
-    subscription.price = subscription.annotated_price
-    subscription.save()
+        time.sleep(20)
 
+        subscription.price = subscription.annotated_price
+        subscription.save()
+
+    print('do something')
+
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    from services.models import Subscription
+
+    print('do something 1')
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+        time.sleep(25)
+
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
+    print('do something 2')
